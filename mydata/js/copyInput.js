f9611987739510842807c1a2b9885ae617c5be79
LucasGinard <contactolucasginard@gmail.com>
2023-06-05 20:48:04 -0400

    init web

---
+++ mydata/js/copyInput.js
@@ -0,0 +1,13 @@
+document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
+    const inputField = copyLinkParent.querySelector(".copy-link-input");
+    const copyButton = copyLinkParent.querySelector(".copy-link-button");
+    const text = inputField.value;
+  
+    inputField.addEventListener("focus", () => inputField.select());
+  
+    copyButton.addEventListener("click", () => {
+      inputField.select();
+      inputField.setSelectionRange(0, 99999); // For mobile devices
+      navigator.clipboard.writeText(text);
+    });
+  });
