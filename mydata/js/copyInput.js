f8c1c7cf28cca1606d87d5b064c6d28f28a09a4f
LucasGinard <contactolucasginard@gmail.com>
2023-07-12 22:33:07 -0400

    add copy link for form

--- mydata/js/copyInput.js
+++ mydata/js/copyInput.js
@@ -1,16 +1,16 @@
 document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
-    const inputField = copyLinkParent.querySelector(".copy-link-input");
-    const copyButton = copyLinkParent.querySelector(".copy-link-button");
-    const text = inputField.value;
-  
-    inputField.addEventListener("focus", () => inputField.select());
-  
-    copyButton.addEventListener("click", () => {
-      inputField.select();
-      inputField.setSelectionRange(0, 99999); // For mobile devices
-      navigator.clipboard.writeText(text);
-    });
+  const inputField = copyLinkParent.querySelector(".copy-link-input");
+  const copyButton = copyLinkParent.querySelector(".copy-link-button");
+
+  inputField.addEventListener("focus", () => inputField.select());
+
+  copyButton.addEventListener("click", () => {
+    let text = inputField.value; // Obtener el valor en el momento de la copia
+    inputField.select();
+    inputField.setSelectionRange(0, 99999); // Para dispositivos móviles
+    navigator.clipboard.writeText(text);
   });
+});
 
 const copyAllButton = document.querySelector(".btn-solid-lg");
 
